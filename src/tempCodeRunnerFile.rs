4ef8ca3c0cc888116fd8367ea5412084c696a78b
Vq-x <74271294+Vq-x@users.noreply.github.com>
2024-08-24 04:20:17 -0400

    Delete src/tempCodeRunnerFile.rs

--- src/tempCodeRunnerFile.rs
+++
@@ -1,5 +0,0 @@
-println!(
-        "one ore: {:?} one ore times total amount: {:?}",
-        hand_of_poseidon.process_ores(&mut vec![ores[0].clone()]),
-        hand_of_poseidon.process_ores(&mut vec![ores[0].clone()]) * ores.len() as f64
-    );
