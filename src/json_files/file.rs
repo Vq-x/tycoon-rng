41be5a5c01a4807548e46103bbb62d99616e53e1
Vq-x <74271294+Vq-x@users.noreply.github.com>
2024-08-27 04:09:37 -0400

    fixed problems with accessing the json file.

--- src/json_files/file.rs
+++ src/json_files/file.rs
@@ -1,8 +1,11 @@
 use std::fs::File;
 use std::io::Read;
+use std::path::PathBuf;
 
 pub fn get_json_text(file_name: &str) -> Result<String, std::io::Error> {
-    let mut file = File::open(file_name)?; // Unwrap the Result using ?
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR")); // This gets the root directory of your project
+    path.push(file_name);
+    let mut file = File::open(path.to_str().expect("the path given was invalid"))?; // Unwrap the Result using ?
     let mut contents = String::new();
     file.read_to_string(&mut contents)?; // Read the file contents into the string
     Ok(contents)
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use serde_json::{from_str, to_string, to_string_pretty};
 use std::{borrow::BorrowMut, vec};
 use types::{
     enums::{
-        FurnaceTypes, Immunities, MineTypes, Modifiers, Multipliers, Tags, UpgraderTypes,
-        Upgraders, Vulnerabilities,
+        FurnaceTypes, Furnaces, Immunities, MineTypes, Mines, Modifiers, Multipliers, Tags,
+        UpgraderTypes, Upgraders, Vulnerabilities,
     },
     furnace::Furnace,
     mine::Mine,
@@ -18,16 +18,20 @@ use types::{
 use utils::human_readable;
 
 fn main() {
-    let furnace = Furnace {
-        multiplier: 47.0,
-        rarity: 101_100,
-        effects: vec![
-            FurnaceTypes::MultiplyIf(1.6, Tags::Magnetic),
-            FurnaceTypes::MultiplyIf(1.6, Tags::Fire(1.0)),
-            FurnaceTypes::MultiplyIf(0.2, Tags::Fueled),
-        ],
-        ..Default::default()
-    };
-    println!("{}", to_string(&furnace).unwrap());
+    let gut = Mine::get_mine(Mines::GuttationDripper, Modifiers::Negative).unwrap();
+    let perfect_lawn = Upgrader::get_upgrader(Upgraders::PerfectLawn, Modifiers::Negative).unwrap();
+    let hand_of_poseidon =
+        Furnace::get_furnace(Furnaces::HandOfPoseidon, Modifiers::Golden).unwrap();
+    let mut ores = gut.spawn_ores(100);
+    ores.upgrade(&perfect_lawn);
+    println!("ores: {:?}", ores.ores);
+    let sum = hand_of_poseidon.process_ores(&mut ores);
+    let one_ore = ores.ores[0].clone();
+    let one = hand_of_poseidon.process_ores(&mut Ores {
+        ores: vec![one_ore],
+    });
+    println!("one: {}", human_readable(one));
+    println!("sum: {}", human_readable(sum / 100.0));
+
     // println!("ores: {:?}", ores.ores);
 }
